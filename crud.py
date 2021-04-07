dc3cac55635457aeb8913f38adbab11c1502655f
Rayna Mecham <rayna.mecham@gmail.com>
2021-04-06 21:54:44 -0600

    worked on part 3

--- crud.py
+++ crud.py
@@ -19,6 +19,16 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """Return all users"""
+
+    return User.query.all()
+
+def get_user_by_id(user_id):
+    """Return a user by id"""
+
+    return User.query.get(user_id)
+
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a new movie."""
 
@@ -31,6 +41,16 @@ def create_movie(title, overview, release_date, poster_path):
     db.session.commit()
 
     return movie
+
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Returns a movie ID"""
+
+    return Movie.query.get(movie_id)
 
 def create_rating(user, movie, score):
     """Create and return a new rating."""
